e900adf82ee8e7f69950b359965e6afaf5cba725
Till Hoffmann <till.hoffmann@enforge.de>
2020-04-08 19:03:37 +0200

    added newline support to ansible_hosts_add

--- ansible/containerdata/web/ansible_hosts_add.php
+++ ansible/containerdata/web/ansible_hosts_add.php
@@ -16,17 +16,20 @@
     $lines = explode("\n",$content);
 
     if(! in_array($clientip,$lines)) {
-        $index = array_search("[nogroup]",$lines);
-        $index = $index;
-        
-        $beginning = array_splice($lines,0,sizeof($lines)-$index);
-        $newlines = array_merge($beginning, array($clientip));
-        $lines = array_merge($newlines, $lines);
+        $index_header = array_search("[nogroup]",$lines); # search for line [nogroup]
+        $beginning = array_splice($lines,0,sizeof($lines)-$index_header); # detach everything from [nogroup] upwards (including [nogroup])
+
+        $index_empty = array_search("",$lines); # search for first empty line after [nogroup]
+        $second_beginning = array_splice($lines,0,sizeof($lines)-$index_empty-1); # detach everything from first empty line upwards (excluding empty line)
+        $beginning = array_merge($beginning, $second_beginning); # merging everything before newlines into beginning
+
+        $newlines = array_merge($beginning, array($clientip)); # appending the new line to beginning and put result in newlines
+        $lines = array_merge($newlines, $lines); # merge newlines and lines
+
+        $content = implode("\n",$lines); # merge array of lines with linebreaks
+
+        $handle = fopen($ansible_hosts_file, 'w') or die('Cannot open file:  '.$ansible_hosts_file); # open file with 'w' permission
+        fwrite($handle, $content); # write to file
+        fclose($handle); # close filehandler
     }
-
-    $content = implode("\n",$lines);
-
-    $handle = fopen($ansible_hosts_file, 'w') or die('Cannot open file:  '.$ansible_hosts_file);
-    fwrite($handle, $content);
-    fclose($handle);
 ?>
