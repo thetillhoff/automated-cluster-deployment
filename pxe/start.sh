fd17f3e0512172cb29b45cffd20db9dc7bb453cf
Till Hoffmann <till.hoffmann@enforge.de>
2020-04-08 21:49:35 +0200

    moved tftp files to own folder

--- pxe/start.sh
+++ pxe/start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-## create /container/public/boot.ipxe with current ip
+## create /container/tftp/boot.ipxe with current ip
 # note that initrd is used as "ifexists"
 # if multiple macs exists maybe ${net0/mac:hexhyp} should be used
-echo -e "#!ipxe\n" > /container/web/boot.ipxe
-echo -e "set boot-url http://$(cut -d' ' -f1 <<<$(hostname -I))/\n" >> /container/web/boot.ipxe
-echo -e "initrd \${boot-url}mac/\${mac:hexhyp}_lastinstalled && chain \${boot-url}boot_harddrive.php?mac=\${mac:hexhyp} ||" >> /container/web/boot.ipxe # if already installed boot from harddrive
-echo -e "chain \${boot-url}mac/\${mac:hexhyp}.php?mac=\${mac:hexhyp} ||" >> /container/web/boot.ipxe # if mac is known and config file exists, load it
-echo -e "chain \${boot-url}default.php?mac=\${mac:hexhyp} ||" >> /container/web/boot.ipxe # else load menu
+echo -e "#!ipxe\n" > /container/tftp/boot.ipxe
+echo -e "set boot-url http://$(cut -d' ' -f1 <<<$(hostname -I))/\n" >> /container/tftp/boot.ipxe
+echo -e "initrd \${boot-url}mac/\${mac:hexhyp}.lastinstalled && chain \${boot-url}boot_harddrive.php?mac=\${mac:hexhyp} ||" >> /container/tftp/boot.ipxe # if already installed, boot from harddrive
+echo -e "chain \${boot-url}mac/\${mac:hexhyp}.php?mac=\${mac:hexhyp} ||" >> /container/tftp/boot.ipxe # if mac is known and config file exists, load it
+echo -e "chain \${boot-url}default.php?mac=\${mac:hexhyp} ||" >> /container/tftp/boot.ipxe # else load menu
 echo -e "shell" # else drop to shell
 
 ## set ip in dnsmasq.conf
