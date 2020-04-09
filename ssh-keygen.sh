fd7828108af80acc703970bc2e97e6fea2bad773
Till Hoffmann <till.hoffmann@enforge.de>
2020-04-09 04:45:23 +0200

    further improvements on ssh-keygen script

--- ssh-keygen.sh
+++ ssh-keygen.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-read -p "Please enter your email for the new ssh-key:" email
+read -p "Please enter your email for the new ssh-key: " email
 ssh-keygen -b 4096 -t rsa -f ./key -C "$email" -N "" <<< y
+echo "" # add newline afterwards
