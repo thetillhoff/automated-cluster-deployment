083766b1cd80b9d5e469875b7d56b2d5917309d2
Till Hoffmann <till.hoffmann@enforge.de>
2020-04-09 20:51:05 +0200

    adding ssh login script

---
+++ ssh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p "Please enter the host you want to connect to: " host
+read -p "Please enter the user you want to be: [enforge] " user
+user=${user:=enforge}  # if variable not set or null, set it to default
+
+ssh -i ./key "$user@$host"
