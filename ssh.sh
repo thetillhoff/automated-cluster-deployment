e37c464e61e0bfa07c77bd06bd4c30bdab71667e
Till Hoffmann <till.hoffmann@enforge.de>
2020-04-10 00:02:24 +0200

    remove hostkeychecking on ssh script

--- ssh.sh
+++ ssh.sh
@@ -4,4 +4,4 @@ read -p "Please enter the host you want to connect to: " host
 read -p "Please enter the user you want to be: [enforge] " user
 user=${user:=enforge}  # if variable not set or null, set it to default
 
-ssh -i ./key "$user@$host"
+ssh -i ./key -o StrictHostKeyChecking=no "$user@$host"
