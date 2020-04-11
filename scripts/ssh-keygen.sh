38bf1b6195d9096f18ee6c1cdf826e0ef47daf6e
Till Hoffmann <till.hoffmann@enforge.de>
2020-04-11 02:17:41 +0200

    added chmod +x and fixed paths to keyfile

--- scripts/ssh-keygen.sh
+++ scripts/ssh-keygen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 read -p "Please enter your email for the new ssh-key: " email
-ssh-keygen -b 4096 -t rsa -f ../key -C "$email" -N "" <<< y
+ssh-keygen -b 4096 -t rsa -f ./key -C "$email" -N "" <<< y
 echo "" # add newline afterwards
