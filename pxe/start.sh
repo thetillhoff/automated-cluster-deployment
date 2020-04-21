27f71ffd3ce7a86fdbed1b15e96122c750f68172
Till Hoffmann <till.hoffmann@enforge.de>
2020-04-22 01:04:19 +0200

    boot.ipxe now loads via http & task edits

--- pxe/start.sh
+++ pxe/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-## allow nginx to create files in /container/web/mac folder
-chmod -R o+w /container/web/mac
+## configure dnsmasq for local ip
+sed -i "s%dhcp-boot=tag:ipxe,http://xxx.xxx.xxx.xxx/default%dhcp-boot=tag:ipxe,http://$(cut -d' ' -f1 <<<$(hostname -I))/default?mac=\${net0/mac:hexhyp}%" /etc/dnsmasq.conf
+
 ## allow nginx to read files from /key folder
 chmod -R o+r /key
 ## allow nginx to write to ansible-hosts file
