d7e7f8c3ecc80733ed489d31caca3e0abcb74253
Till Hoffmann <till.hoffmann@enforge.de>
2020-05-24 00:35:42 +0200

    added encryption for keys

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -4,4 +4,4 @@ read -p "Please enter the host you want to connect to: " host
 read -p "Please enter the user you want to be: [enforge] " user
 user=${user:=enforge}  # if variable not set or null, set it to default
 
-ssh -i ./key -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" -o "LogLevel=error" "$user@$host"
+ssh -i ./key.secret -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" -o "LogLevel=error" "$user@$host"
